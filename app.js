68733dcbb91efb7769d38f2aac9b58e7a2f0a5f8
Jonathan Wideman <jonathan.m.wideman@gmail.com>
2019-07-15 21:23:46 -0400

    Revert "Add switching between Test and Time dice"

--- app.js
+++ app.js
@@ -4,16 +4,10 @@ cfg = {
         skull: 'images/skull.png',
         singleHit: 'images/singleHit.png',
         doubleHit: 'images/doubleHit.png',
-        tu1: 'images/tu1.png',
-        tu2: 'images/tu2.png',
-        tu3: 'images/tu3.png',
-        testDie: 'images/singleHit.png',
-        timeDie: 'images/tu1.png',
     }
 }
 
-testDie = {
-    name: 'testDie',
+die = {
     faces: [
         'blank',
         'doubleHit',
@@ -24,57 +18,31 @@ testDie = {
     ]
 }
 
-timeDie = {
-    name: 'timeDie',
-    faces: [
-        'tu1',
-        'tu2',
-        'tu2',
-        'tu2',
-        'tu2',
-        'tu3',
-    ]
-}
-
 rollHistoryMax = 20
 
-dieToRoll = testDie
 numToRollMax = 6
 numToRoll = numToRollMax
-updateControls()
+updateRollButton()
 
 function OnPlusButtonClicked() {
     numToRoll = Math.min(numToRoll + 1, numToRollMax)
-    updateControls()
+        // console.log(numToRoll)
+    updateRollButton()
 }
 
 function OnMinusButtonClicked() {
     numToRoll = Math.max(1, numToRoll - 1)
-    updateControls()
+        // console.log(numToRoll)
+    updateRollButton()
 }
 
 function OnRollButtonClicked() {
     var result = ''
-    if (dieToRoll == testDie) {
-        RollDice(Array(numToRoll).fill({...dieToRoll })).forEach(dieToRoll => {
-            result += outputDie(dieToRoll)
-        })
-    } else {
-        RollDice(Array(1).fill({...dieToRoll })).forEach(dieToRoll => {
-            result += outputDie(dieToRoll)
-        })
-    }
+        // RollDice(Array(parseInt(numDice.value)).fill({...die })).forEach(die => {
+    RollDice(Array(numToRoll).fill({...die })).forEach(die => {
+        result += outputImage(die)
+    })
     pushResult(result)
-}
-
-function OnTestDieButtonClicked() {
-    dieToRoll = testDie
-    updateControls()
-}
-
-function OnTimeDieButtonClicked() {
-    dieToRoll = timeDie
-    updateControls()
 }
 
 function RollDice(dice) {
@@ -91,13 +59,14 @@ function chooseRandomFrom(items) {
     return items[Math.floor(Math.random() * (max - min)) + min]
 }
 
-function outputDie(name, className = 'die') {
-    return '<img class="' + className + '" src="' + cfg.images[name] + '">'
+function outputImage(name) {
+    return '<img class="die" src="' + cfg.images[name] + '">'
 }
 
 function pushResult(result) {
     var li = document.createElement("li")
     li.innerHTML = result
+        // li.className = 'rollResult'
 
     if (rolls.firstChild.children != null) {
         ([...rolls.firstChild.children]).forEach(element => {
@@ -112,22 +81,6 @@ function pushResult(result) {
     }
 }
 
-function updateControls() {
-    if (dieToRoll == testDie) {
-        testDieButtonImage.className = "die-button"
-        timeDieButtonImage.className = "die-button-inactive"
-
-        rollButton.innerHTML = "Roll " + numToRoll
-
-        minusButton.removeAttribute("disabled")
-        plusButton.removeAttribute("disabled")
-    } else {
-        testDieButtonImage.className = "die-button-inactive"
-        timeDieButtonImage.className = "die-button"
-
-        rollButton.innerHTML = "Roll"
-
-        minusButton.setAttribute("disabled", "disabled")
-        plusButton.setAttribute("disabled", "disabled")
-    }
+function updateRollButton() {
+    rollButton.innerHTML = "Roll " + numToRoll
 }
